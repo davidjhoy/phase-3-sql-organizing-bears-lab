69cd4524132ebc12d030333bce889ccd19aaee81
david <davidjhoyjr@gmail.com>
2022-07-03 08:16:25 -0400

    commit tree 06860d91c514ac2e4cae4f428d01d372b90b4de9

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Maru', 3, "M", "Blue", 'Scottish Fold', 'Yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Haru', 3, "M", "Blue", 'Scottish Fold', 'Yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Laru', 4, "M", "Blue", 'Scottish Fold', 'Yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Daru', 4, "M", "Blue", 'Scottish Fold', 'Yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Zaru', 5, "M", "Blue", 'Scottish Fold', 'Yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Aaru', 2, "M", "Blue", 'Scottish Fold', 'Yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Saru', 1, "M", "Blue", 'Scottish Fold', 'Yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, "M", "Blue", 'Scottish Fold', 'Yes');
